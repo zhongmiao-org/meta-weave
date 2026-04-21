6714b47b2370b066f2059e008431d1f718775d2f
ark-65 <liuwufangzhou@vip.qq.com>
2026-04-21 14:11:19 +0800

    add local ngx-lowcode link workflow and fix tethys ui

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  inject,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { ThyIconRegistry } from 'ngx-tethys/icon';
 import { routes } from '../../app.routes';
+
+function provideTethysIcons() {
+  return () => {
+    const iconRegistry = inject(ThyIconRegistry);
+    iconRegistry.addSvgIconSet('/assets/icons/defs/svg/sprite.defs.svg');
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +23,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimations(),
     provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: provideTethysIcons,
+    },
   ],
 };
